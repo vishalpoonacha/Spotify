04f15c8cdf634321f9fd7fd3c788d401bedf8d71
vishalpoonacha <vishalpoonacha007@gmail.com>
2021-05-03 23:24:11 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-
+import React,{ useEffect, useState } from 'react';
 import './App.css';
 import Login from './Login';
+import { getTokenFromUrl } from './spotify';
 
 function App() {
+
+  const [token, setToken] = useState(null);
+
+  useEffect(() =>{
+    const hash = getTokenFromUrl();
+    window.location.hash="";
+
+    const _token =hash.access_token
+
+    if(_token){
+      setToken(_token)
+    }
+
+    console.log('>>>',token);
+  },[])
+
+
   return (
     <div className="app">
+      {
+        token ? (
+          <h1>I am Logged In </h1>
+        ):(
+          <Login/>
+        )
+      }
      <Login/>
     </div>
   );
